79ac761d803f446c035602ebfde1d6f1a2046e17
乔彬 <120476536@qq.com>
2024-05-07 11:47:19 +0800

    将aar文件上传到 阿里云maven

---
+++ app/src/main/java/com/linji/demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.linji.demo
+
+import android.app.Activity
+import android.os.Bundle
+import com.linji.basecabinet.R
+
+class MainActivity :Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
