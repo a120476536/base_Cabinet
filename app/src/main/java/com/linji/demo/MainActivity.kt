6896680d96f62b2cf2108189eeb9bf1c0e0fca40
乔彬 <120476536@qq.com>
2024-05-07 14:44:38 +0800

    1.0.006

--- app/src/main/java/com/linji/demo/MainActivity.kt
+++ app/src/main/java/com/linji/demo/MainActivity.kt
@@ -3,10 +3,12 @@ package com.linji.demo
 import android.app.Activity
 import android.os.Bundle
 import com.linji.basecabinet.R
+import com.linji.mylibrary.utils.HandlerUtil
 
 class MainActivity :Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        HandlerUtil.instance.removeCallbacksAndMessages(null)
     }
 }
